0e24c3722046dfad198d711454105a27a3a571cc
hamster1963 <1410514192@qq.com>
2024-12-02 17:53:07 +0800

    fix: baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     react(),
     VitePWA({
@@ -33,7 +34,10 @@ export default defineConfig({
             purpose: "any maskable"
           }
         ]
-      }
+      },
+      workbox: {
+        navigateFallbackDenylist: [/^\/dashboard/],
+      },
     })
   ],
   resolve: {
