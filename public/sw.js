05bb29534963150a8e0e2828a42e8d41e12b74a1
hamster1963 <1410514192@qq.com>
2024-12-09 00:28:19 +0800

    fix: remove service worker

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+/**
+ * Self-Destroy service worker
+ */
+
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll();
+    })
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url));
+    });
+});
