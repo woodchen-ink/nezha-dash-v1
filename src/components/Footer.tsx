1483ce56fa3c76342ef06a4ceebd37c32c5daca7
hamster1963 <1410514192@qq.com>
2024-12-12 20:52:30 +0800

    fix: footer overflow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="mx-auto w-full max-w-5xl px-4 lg:px-0 pb-4">
       <section className="flex flex-col">
-        <section className="mt-1 flex items-center justify-between gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
+        <section className="mt-1 flex items-center sm:flex-row flex-col justify-between gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           <div className="flex items-center gap-1">
             &copy;2020-{new Date().getFullYear()}{" "}
             <a href={"https://github.com/naiba/nezha"} target="_blank">
