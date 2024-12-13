70d69289871f269562d5b0c6653f38e460f4005a
hamster1963 <1410514192@qq.com>
2024-12-13 17:31:22 +0800

    feat: pr lint

--- eslint.config.js
+++ eslint.config.js
@@ -19,9 +19,10 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-refresh/only-export-components": "off",
       "react-hooks/exhaustive-deps": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 )
