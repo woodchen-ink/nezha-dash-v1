3f7eeeca84829150cc83bbfd9dcea0bff38bd32d
Forget <fhs8825@gmail.com>
2024-12-21 21:21:42 +0800

    feat: 添加部分节点语义化class

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("rounded-lg border bg-card text-card-foreground shadow-lg shadow-neutral-200/40 dark:shadow-none", className)}
+    className={cn("rounded-lg border bg-card text-card-foreground shadow-lg shadow-neutral-200/40 dark:shadow-none server-network-info", className)}
     {...props}
   />
 ))
